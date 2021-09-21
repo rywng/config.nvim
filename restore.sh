2b541bdcb4f2154526bfea4033f6d818e9406529
juan <juancloudcomputing@protonmail.com>
2021-09-21 11:56:18 +0800

    remove test scripts since its stable now

--- restore.sh
+++
@@ -1,5 +0,0 @@
-mv nvim nvim.new
-mv nvim.bak nvim
-mv /home/juan/.local/share/nvim/site/pack /home/juan/.local/share/nvim/site/pack.new
-mv /home/juan/.local/share/nvim/site/pack.bak /home/juan/.local/share/nvim/site/pack
-
