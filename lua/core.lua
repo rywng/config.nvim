2b5622785838c2de339e0a35187cd6582b9fce9a
juan <juancloudcomputing@protonmail.com>
2021-11-11 10:46:45 +0800

    add cursorline

--- lua/core.lua
+++ lua/core.lua
@@ -5,6 +5,7 @@ local global = vim.g
 -- look and feel
 vim.opt.shortmess:append({ I = true }) -- remove intro message
 opt.relativenumber = true
+opt.cursorline = true
 opt.number = true
 opt.showmode = false
 opt.scrolloff = 10
