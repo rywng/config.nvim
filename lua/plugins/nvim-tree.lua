9897c9665aab5a6f06f33d7b24e90a1a7fc3a49b
juan <juancloudcomputing@protonmail.com>
2022-06-27 15:47:14 +0800

    Fix | Update nvim-tree configuration

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,13 +2,13 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
     disable_netrw = true,
-    hijack_netrw = true,
     open_on_setup = false,
     ignore_ft_on_setup = {},
     open_on_tab = true,
     hijack_cursor = true,
+    hijack_directories = {enable = true, auto_open = true},
+    hijack_netrw = true,
     update_cwd = true,
-    update_to_buf_dir = {enable = true, auto_open = true},
     diagnostics = {
         enable = true,
         icons = {hint = "", info = "", warning = "", error = ""}
