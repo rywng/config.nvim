a267b027d65ec924666c082df229b32e9d6a61c4
juan <juancloudcomputing@protonmail.com>
2023-03-09 17:54:51 +0800

    nvim-autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,8 +1,14 @@
-local npairs = require('nvim-autopairs')
+local npairs = require("nvim-autopairs")
 
-npairs.setup({map_bs = true, map_cr = true, map_c_w = true})
+npairs.setup({
+	map_bs = true,
+	map_cr = true,
+	map_c_w = true,
+	enable_check_bracket_line = true,
+	check_ts = true,
+})
 
--- -- If you want insert `(` after select function or method item
--- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
--- local cmp = require('cmp')
--- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
