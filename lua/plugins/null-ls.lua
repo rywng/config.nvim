1145f64b931f75746795e223422fa30b3b185a33
Ryan <ryan@alien.gov>
2024-01-03 13:16:08 +0800

    Update default gitsigns source

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,13 +1,15 @@
 require("mason").setup()
 require("mason-null-ls").setup({
-    ensure_installed = {
-        -- Opt to list sources here, when available in mason.
-    },
-    automatic_installation = false,
-    handlers = {},
+	ensure_installed = {
+		-- Opt to list sources here, when available in mason.
+	},
+	automatic_installation = false,
+	handlers = {},
 })
-require("null-ls").setup({
-    sources = {
-        -- Anything not supported by mason.
-    }
+
+local null_ls = require("null-ls")
+null_ls.setup({
+	sources = {
+		null_ls.builtins.code_actions.gitsigns,
+	}
 })
