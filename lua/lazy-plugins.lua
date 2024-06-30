bbf2e34a2c6881edf5bc0b62c4396951bec71e5e
Ryan <ryan@alien.gov>
2024-06-30 12:15:49 +0800

    update color scheme settings

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -33,8 +33,7 @@ local plugins = {
 		config = function()
 			vim.g.gruvbox_material_better_performance = true
 			vim.g.gruvbox_material_enable_bold = true
-			vim.g.gruvbox_material_foreground = 1
-			vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+			vim.g.gruvbox_material_enable_italic = true
 			vim.cmd([[colorscheme gruvbox-material]])
 		end
 	},
