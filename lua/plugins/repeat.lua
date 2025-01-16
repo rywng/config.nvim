db6349baa8953a2a3c938799a9b8ec73f6efb741
Ryan <ryan@alien.gov>
2025-01-16 20:00:51 +0800

    fix(repeat): fix diff ]c repeat not working

--- lua/plugins/repeat.lua
+++ lua/plugins/repeat.lua
@@ -10,23 +10,24 @@ vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = t
 vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 -- Repeat gitsigns
 local next_hunk = function()
-	if vim.wo.diff then return ']c' end
-	vim.schedule(function()
-		require("gitsigns").next_hunk()
-	end)
-	return '<Ignore>'
+	if vim.wo.diff then
+		vim.cmd.normal({ ']c', bang = true })
+	else
+		gitsigns.nav_hunk('next')
+	end
 end
 local prev_hunk = function()
-	if vim.wo.diff then return '[c' end
-	vim.schedule(function()
-		require("gitsigns").prev_hunk()
-	end)
-	return '<Ignore>'
+	if vim.wo.diff then
+		vim.cmd.normal({ '[c', bang = true })
+	else
+		gitsigns.nav_hunk('prev')
+	end
 end
 local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(next_hunk, prev_hunk)
 vim.keymap.set({ "n", "x", "o" }, "]c", next_hunk_repeat)
 vim.keymap.set({ "n", "x", "o" }, "[c", prev_hunk_repeat)
 -- Repeat lsp diagnoistics
-local next_diagnostic, prev_diagnostic = ts_repeat_move.make_repeatable_move_pair(vim.diagnostic.goto_next,vim.diagnostic.goto_prev)
+local next_diagnostic, prev_diagnostic = ts_repeat_move.make_repeatable_move_pair(vim.diagnostic.goto_next,
+	vim.diagnostic.goto_prev)
 vim.keymap.set({ "n", "x", "o" }, "]d", next_diagnostic)
 vim.keymap.set({ "n", "x", "o" }, "[d", prev_diagnostic)
