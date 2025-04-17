078dc615dafbd6c0e2fd3028b91f5f3c4a92bde8
Ryan <ryan@alien.gov>
2025-04-17 17:09:34 +0800

    Conditionally load dxfmt builtin

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,6 +8,10 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.formatting.dxfmt,
+		function()
+			if vim.fn.executable("dx") == 1 then
+				return null_ls.builtins.formatting.dxfmt
+			end
+		end,
 	},
 })
