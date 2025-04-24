98d723e3f43074c319686405d940ed6cff2bc77d
Ryan <ryan@alien.gov>
2025-04-24 20:35:13 +0800

    Remove neotree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -200,19 +200,6 @@ local plugins = {
 			},
 		},
 	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-		event = "VeryLazy",
-		config = function()
-			require("plugins.neo-tree")
-		end,
-	},
 
 	-- Eye candy UI
 	{
