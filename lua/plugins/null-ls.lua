3b902464d0e3edffc8d4ca7e30dd6b08687314f8
Ryan <ryan@alien.gov>
2025-07-24 15:42:01 +0800

    feat: Enable prettierd formatter

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,6 +3,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.diagnostics.selene,
+		null_ls.builtins.formatting.prettierd,
 		function()
 			if vim.fn.executable("dx") == 1 then
 				return null_ls.builtins.formatting.dxfmt
