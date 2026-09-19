6ec72eefdbd27a2c06f3f9e398c48a15ddd14b67
rwong <rwong@noreply.codeberg.org>
2026-09-19 12:16:27 +0200

    null_ls: only load builtin if command found

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,8 +2,16 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.diagnostics.selene,
-		null_ls.builtins.formatting.prettierd,
+		function()
+			if vim.fn.executable("selene") == 1 then
+				return null_ls.builtins.formatting.selene
+			end
+		end,
+		function()
+			if vim.fn.executable("prettierd") == 1 then
+				return null_ls.builtins.formatting.prettierd
+			end
+		end,
 		function()
 			if vim.fn.executable("dx") == 1 then
 				return null_ls.builtins.formatting.dxfmt
